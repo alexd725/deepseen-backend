755bdfd46096f71fc2cba8f92eaacfc306696c64
Alex <alex725.dev@gmail.com>
2020-12-15 22:02:33 +0300

    Working on SendGrid: test email sending

--- utilities/sendgrid.go
+++ utilities/sendgrid.go
@@ -9,16 +9,21 @@ import (
 )
 
 // Send an email via SendGrid
-func SendEmail(address, template string) {
-	from := mail.NewEmail("Deepseen", "test@example.com")
-	subject := "Sending with Twilio SendGrid is Fun"
-	to := mail.NewEmail("Example User", "test@example.com")
-	plainTextContent := "and easy to do anywhere, even with Go"
-	htmlContent := "<strong>and easy to do anywhere, even with Go</strong>"
+func SendEmail(destination, firstName, lastName, subject, template string) {
+	from := mail.NewEmail("Deepseen", os.Getenv("SENDGRID_FROM_ADDRESS"))
+	to := mail.NewEmail(firstName+" "+lastName, destination)
+
+	plainTextContent := "This is a test"
+	htmlContent := "<strong>THIS is a test</strong>"
 	message := mail.NewSingleEmail(from, subject, to, plainTextContent, htmlContent)
+
 	client := sendgrid.NewSendClient(os.Getenv("SENDGRID_API_KEY"))
 	response, emailError := client.Send(message)
+
+	// show log
 	if emailError == nil {
-		fmt.Println("-- mailer: sent to ", address, "[", response.StatusCode, response.Body, "]")
+		fmt.Println("-- SENDGRID: sent to", destination, "[", response.StatusCode, response.Body, "]")
+	} else {
+		fmt.Println("-- SENDGRID: error", emailError, "[", destination, "]")
 	}
 }
